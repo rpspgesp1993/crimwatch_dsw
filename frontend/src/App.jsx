bdb9ff37e9404b0bcc0bfb8a4ee5f3326b9bf744
rpspgesp1993 <161170321+rpspgesp1993@users.noreply.github.com>
2025-05-25 11:41:56 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Ranking from './pages/Ranking/Ranking'; // Importe o componente Ranking
 import 'leaflet/dist/leaflet.css';
 import './styles/main.css';
 
@@ -8,6 +9,7 @@ function App() {
   return (
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/ranking" element={<Ranking />} /> {/* Adicione esta linha para a página de ranking */}
       </Routes>
   );
 }
