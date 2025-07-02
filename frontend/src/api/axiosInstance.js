f896a47c54eb2f1d78eec70c67a552e453cc67b5
diego <diego@gmail.com>
2025-07-02 11:44:45 -0300

    preparing for deploy

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:4000/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
